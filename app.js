25638b4c1a0fa3c955d6d6c6bdb5eff8122ebc7b
ian <47257912+ianjamesx@users.noreply.github.com>
2020-03-08 14:48:38 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res) {
+
+    res.sendFile(__dirname + '/client/index.html');
+
+});
+
+app.use('/client', express.static(__dirname + '/client'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+serv.listen(9000);
+
+console.log('editor online on port 9000');
